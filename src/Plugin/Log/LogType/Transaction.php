5a3961e348ce8d6c67cceabd41c1d0d1c6f76cb5
Michael Stenta <mike@mstenta.net>
2021-09-20 21:02:30 -0400

    Merge Sale and Purchase logs into Transaction log.

--- src/Plugin/Log/LogType/Transaction.php
+++ src/Plugin/Log/LogType/Transaction.php
@@ -14,4 +14,57 @@ use Drupal\farm_entity\Plugin\Log\LogType\FarmLogType;
  */
 class Transaction extends FarmLogType {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function buildFieldDefinitions() {
+    $fields = parent::buildFieldDefinitions();
+
+    // Invoice number.
+    $options = [
+      'type' => 'string',
+      'label' => $this->t('Invoice number'),
+      'weight' => [
+        'form' => 20,
+        'view' => 20,
+      ],
+    ];
+    $fields['invoice_number'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    // Customer.
+    $options = [
+      'type' => 'string',
+      'label' => $this->t('Customer'),
+      'weight' => [
+        'form' => 20,
+        'view' => 20,
+      ],
+    ];
+    $fields['customer'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    // Seller.
+    $options = [
+      'type' => 'string',
+      'label' => $this->t('Seller'),
+      'weight' => [
+        'form' => 20,
+        'view' => 20,
+      ],
+    ];
+    $fields['seller'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    // Lot number.
+    $options = [
+      'type' => 'string',
+      'label' => $this->t('Lot number'),
+      'weight' => [
+        'form' => 20,
+        'view' => 20,
+      ],
+    ];
+    $fields['lot_number'] = $this->farmFieldFactory->bundleFieldDefinition($options);
+
+    return $fields;
+  }
+
 }
